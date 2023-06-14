5c6a9d2998fa594baff9546bfea776dc1792d276
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-06-14 11:18:58 +0700

    fix product component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,6 +1,6 @@
-import { ReactElement } from 'react';
-import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
-import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
+import { ReactElement } from "react";
+import { Box, SimpleGrid, Icon, Text, Stack, Flex } from "@chakra-ui/react";
+import { FcAssistant, FcDonate, FcInTransit } from "react-icons/fc";
 
 interface FeatureProps {
   title: string;
@@ -9,54 +9,64 @@ interface FeatureProps {
   bg: string;
 }
 
+const featuresList = [
+  {
+    id: 0,
+    icon: FcAssistant,
+    title: "Tư vấn giải pháp",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...",
+    bgColor: "linear(black)",
+  },
+  {
+    id: 1,
+    icon: FcDonate,
+    title: "Giải pháp nhân sự",
+    text: "Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata",
+    bgColor: "linear(black, #2e2e2e, black)",
+  },
+  {
+    id: 2,
+    icon: FcInTransit,
+    title: "Xây dựng phần mềm",
+    text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    bgColor: "linear(black)",
+  },
+];
+
 const Feature = ({ title, text, icon, bg }: FeatureProps) => {
   return (
-    <Stack>
+    <Stack height={"full"} p={5} bgGradient={bg}>
       <Flex
         w={16}
         h={16}
-        align={'center'}
-        justify={'center'}
-        color={'white'}
-        rounded={'full'}
-        bg={'black'}
-        mb={1}>
+        align={"center"}
+        justify={"center"}
+        color={"white"}
+        rounded={"full"}
+        mb={1}
+      >
         {icon}
       </Flex>
-      <Text fontWeight={600} color={'white'}>{title}</Text>
-      <Text color={'white'}>{text}</Text>
+      <Text fontWeight={600} color={"white"}>
+        {title}
+      </Text>
+      <Text color={"white"}>{text}</Text>
     </Stack>
   );
 };
 
 export default function Products() {
   return (
-    <Box p={10} pl={200} pr={200} bg='black' >
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-        <Feature
-          icon={<Icon as={FcAssistant} w={10} h={10} />}
-          title={'Tư vấn giải pháp'}
-          text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-          }
-          bg='black'
-        />
-        <Feature
-          icon={<Icon as={FcDonate} w={10} h={10} />}
-          title={'Giải pháp nhân sự'}
-          text={
-            'Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata'
-          }
-          bg='grey.80'
-        />
-        <Feature
-          icon={<Icon as={FcInTransit} w={10} h={10} />}
-          title={'Xây dựng phần mềm'}
-          text={
-            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
-          }
-          bg='black'
-        />
+    <Box pl={150} pr={150} bg="black">
+      <SimpleGrid columns={{ base: 1, md: 3 }}>
+        {featuresList.map((feature) => (
+          <Feature
+            icon={<Icon as={feature.icon} w={10} h={10} />}
+            title={feature.title}
+            text={feature.text}
+            bg={feature.bgColor}
+          />
+        ))}
       </SimpleGrid>
     </Box>
   );
